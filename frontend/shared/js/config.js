c0a1d089d8e2e7fba33b11d2284d5d6cc067afef
Philip Baumgärtner <philip_baumgaertner@hotmail.de>
2025-06-04 05:45:37 +0200

    release

--- frontend/shared/js/config.js
+++ frontend/shared/js/config.js
@@ -3,7 +3,7 @@ const GUEST_LOGIN = {
     "password": "asdasdasd"
 }
 
-const API_BASE_URL = 'http://127.0.0.1:8000/api/';      /* bei Release ändern */
+const API_BASE_URL = window.location.origin + '/api/';
 
 const LOGIN_URL = 'login/';
 
